1977dfc26f8ec1ddf5915cf945dfd8ec965e35e5
Liqiang Yang <1871649909yang@gmail.com>
2025-01-20 20:01:24 +0100

    Added splash and on boarding page

---
+++ lib/pages/onboarding/onboarding.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final PageController _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            children: [
+              Container(
+                color: Colors.blue,
+              ),
+              Container(
+                color: Colors.purple,
+              ),
+              Container(
+                color: Colors.white,
+              )
+            ],
+          ),
+          Container(
+            alignment: const Alignment(0, 0.75),
+            child: SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: const WormEffect(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
