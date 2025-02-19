73bc27345013e1a8099e1d4d4be510164861539b
JordiDago23 <148897047+JordiDago23@users.noreply.github.com>
2025-02-19 17:01:16 +0100

    a memuelo

--- lib/pages/tablet/login.dart
+++ lib/pages/tablet/login.dart
@@ -1,36 +1,43 @@
+import 'package:dister/generated/l10n.dart';
+import 'package:dister/pages/mobile/onboarding/intropage.dart';
 import 'package:flutter/material.dart';
 
 class LoginTab extends StatefulWidget{
+  const LoginTab({super.key});
 
- @override
+  @override
   State<LoginTab> createState() => _LoginTabState();
-
 }
 
 class _LoginTabState extends State<LoginTab>{
 
+  
+
+
 @override
   Widget build(BuildContext context) {
    
+    double height = MediaQuery.of(context).size.height;
+    double widht = MediaQuery.of(context).size.width;
+
     return Scaffold(
-    body: Expanded(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                  Image.asset("assets/images/intropage/background1.png",  ),
-              ], 
-            ),
-            Column(
-
-              children: [
-                Image.asset("assets/images/intropage/background1.png"),
-              ],
-
-            ),
-          ],
+      backgroundColor: Theme.of(context).colorScheme.surface,
+    body:Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: height,
+          child: Intropage(
+                background: 'assets/images/intropage/background1.png',
+                title: S.of(context).title_onboarding,
+                subtitles: S.of(context).subtitle_onboarding,
+              ),
         ),
-    ),
+        
+        
+      ]
+    ), 
 
     );
   }
