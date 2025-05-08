d33addddb540725bf82ce4214ee2a57169084cde
MarcHunter87 <marchunter888@gmail.com>
2025-05-08 14:48:51 +0200

    update

--- lib/screens/mobile/posts/api_posts_screen.dart
+++ lib/screens/mobile/posts/api_posts_screen.dart
@@ -262,7 +262,7 @@ class _ApiPostsState extends State<ApiPostsScreen> {
                       crossAxisCount: crossAxisCount,
                       crossAxisSpacing: 10,
                       mainAxisSpacing: 10,
-                      childAspectRatio: 0.85,
+                      childAspectRatio: 0.675,
                     ),
                     delegate: SliverChildBuilderDelegate(
                       (context, index) {
